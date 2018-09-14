716bbfc7de90cbc9c393b688dff0d7af70d52bb8
JQueen99 <43067481+JQueen99@users.noreply.github.com>
2018-09-14 15:11:04 +0100

    Added a timer

--- Assets/Scriptsssss/ButtonMasher.cs
+++ Assets/Scriptsssss/ButtonMasher.cs
@@ -6,20 +6,34 @@ public class ButtonMasher : MonoBehaviour {
 
     public AudioSource clickSound; // sound that plays when button clicks
     public TextMesh scoreText; // display player score
-
+    public TextMesh timerText; // display time left
 
     private int score = 0; // Numerical value of player score
+    
+
+    public float gameLength; // How long game lasts
+    public float timeRemaining = 0;
 
 	// Use this for initialization
 	void Start () {
 
         Debug.Log("Start function called");
-		
+
+
+        timeRemaining = gameLength;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Debug.Log("Start function called");
+
+        timeRemaining = timeRemaining - Time.deltaTime;
+
+
+
+        timerText.text =  (Mathf.CeilToInt (timeRemaining)).ToString();
+
     }
 
     private void OnMouseDown() // Function for when mouse is clicked
