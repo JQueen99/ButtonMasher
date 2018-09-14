4fda158567490db92af9282d5d8c1f24bf5c92e6
JQueen99 <43067481+JQueen99@users.noreply.github.com>
2018-09-14 13:57:23 +0100

    Added music and button click sounds

--- Assets/Scriptsssss/ButtonMasher.cs
+++ Assets/Scriptsssss/ButtonMasher.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ButtonMasher : MonoBehaviour {
 
+    public AudioSource clickSound; // sound that plays when button clicks
+
+
 	// Use this for initialization
 	void Start () {
 
@@ -13,6 +16,17 @@ public class ButtonMasher : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-}
+        Debug.Log("Start function called");
+    }
+
+    private void OnMouseDown() // Function for when mouse is clicked
+    {
+        Debug.Log("Mouse down function");
+        // Activates clicksound when buton is clicked
+
+        clickSound.Play();
+
+    } // End of OnMouseDown
+} // End of ButtonMasher class
+
+
